384d5c0cb264fc452db910d6e1be13cc126077d2
shankarojha <shankarojha34@gmail.com>
2021-08-22 03:04:05 +0530

    2nd-commit

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public userEmail = 'shankarojha34@gmail.com'
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,13 +6,15 @@ import { UserComponent } from './user.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordresetComponent } from './passwordreset/passwordreset.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [UserComponent, SignupComponent, LoginComponent, PasswordresetComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    SharedModule
   ]
 })
 export class UserModule { }
